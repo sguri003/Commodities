a5e44e9eb80d351d00e2d70a2948da4f584b891e
smguri002 <57817751+smguri002@users.noreply.github.com>
2025-10-21 13:02:24 -0400

    updates

--- Prac.py
+++ Prac.py
@@ -16,7 +16,7 @@ def rd_fl():
     df['YR'] = (df['Date'][0:4])
     #print(df.sort_values(by='YR' , ascending=True))
     #.loc to slice multipe rows and columns
-    print(df.loc[1:33,['Series ID', 'Date']])
+    print(df.loc[1:33   ,['Series ID', 'Date']])
     df_agg = df.groupby('Series ID').agg({'Annual_Per'
                                           :['max', 'sum'], 'Value': ['min', 'sum']})
     print(df_agg.info())
@@ -27,17 +27,28 @@ def df_agg():
     df = pd.DataFrame(dt)
     print(df.loc[1:22,['Date', 'Series ID']])
     df_agg = df.groupby('Series ID').agg({'Value' :'min'})
-    print(df_agg)
+    print(df.loc[1:20,['Series ID', 'Date']])
+    #print(df_agg)
+    #get all gas/fuel index
     df_g =df[df['Series ID']=='CUSR0000SETB01']
     print(df_g)
+    
+    
 def rd_csv():
     lst = []
+    wrtr = open(FOUT, 'w')
     with open(FIN, 'r')as f:
-        for row in f:
-            #print(row.strip())
-            lst.append(row.strip())
-        i=0
-        for x in lst:
-            print(x.strip(','))
+        rd = csv.reader(f)
+        for row in rd:
+            if row[1].endswith('B01'): 
+                lst.append(row)
+    wtr = csv.writer(wrtr, lineterminator='\n')
+    for x in lst:
+        print(x)
+        wtr.writerow(x)
     
-df_agg()
+    wrtr.close()
+
+            
+    
+rd_csv()
